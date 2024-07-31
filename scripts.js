52f08a539ec43b5659a102d2984deaee9d896944
Dupopking <dupopking@gmail.com>
2024-07-31 10:53:59 -0400

    added clear button and started working on actionlistener

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
+const DEFAULT_COLOR = '#000000'
+const DEFAULT_SIZE = 16
 
-const grid_size = 16
+const currSize = 16
 const grid = document.getElementById("grid");
+
+console.log("start of java script");
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+const clearBtn = getElementById("clearBtn");
+clearBtn.onclick = () => reloadGrid();
+console.log("I'm in")
+
+function reloadGrid() {
+    //clearGrid()
+    initializeGridGrid(currSize);
+}
 
 
 function initializeGrid(size){
-    for(i=0;i<grid_size;i++){
-        for(j=0;j<grid_size;j++){
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            console.log("in loop");
+
             const gridElement = document.createElement("div");
             gridElement.classList.add("grid-element");
             gridElement.addEventListener("mouseover",changeColor);
@@ -15,6 +34,13 @@ function initializeGrid(size){
     }
 }
 
-function chnageColor(e){
-    
+function changeColor(e){
+    if(e.type=="mouseover"&& !mouseDown){
+        return;
+    }
+    e.target.style.backgroundColor = DEFAULT_COLOR;
+}
+
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
 }
